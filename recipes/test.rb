54449771f7fea085109b29eb2fa3f2c0bc45cb30
Jim Dowling <jdowling@sics.se>
2015-03-21 12:05:17 +0100

    kagent_param not working for multiple output params with same cookbook/recipe prefix

--- recipes/test.rb
+++ recipes/test.rb
@@ -6,3 +6,17 @@ kagent_param "/tmp" do
   param "param"
   value "value"
 end
+
+kagent_param "/tmp" do
+  cookbook "kagent"
+  recipe "test"
+  param "param2"
+  value "value2"
+end
+
+kagent_param "/tmp" do
+  cookbook "ndb"
+  recipe "mgmd"
+  param "opensshkey"
+  value "xxxxxxxxxxxxxxxxxxxx"
+end
