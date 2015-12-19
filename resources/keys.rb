b7745ebad049badf163570f847d362b979c7b8fc
Jim Dowling <jdowling@sics.se>
2015-12-20 00:17:45 +0100

    updates

--- resources/keys.rb
+++ resources/keys.rb
@@ -1,7 +1,7 @@
 actions :generate, :return_publickey, :get_publickey
 
 attribute :homedir, :kind_of => String, :name_attribute => true, :required => true
-attribute :cb_naem, :kind_of => String
+attribute :cb_name, :kind_of => String
 attribute :cb_recipe, :kind_of => String
 attribute :cb_user, :kind_of => String, :required => true
 attribute :cb_group, :kind_of => String, :required => true
