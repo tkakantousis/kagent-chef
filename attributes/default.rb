edb30ac8f374a6603f94ab60b4120dff5d275138
Jim Dowling <jdowling@sics.se>
2016-08-03 10:12:08 +0200

    fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,7 +40,7 @@ default.kagent.dashboard_app          = "hopsworks"
 # local settings for agent
 default.kagent.port                   = 8090
 default.kagent.heartbeat_interval     = 10
-default.kagent.watch_interval         = 2
+default.kagent.watch_interval         = 5
 default.kagent.pid_file               = node.kagent.base_dir + "/hop-agent.pid"
 default.kagent.logging_level          = "INFO"
 default.kagent.max_log_size           = "10000000"
