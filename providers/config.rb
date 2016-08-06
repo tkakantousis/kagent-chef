c4cf16845dad9b9b50c6243f8dee39f0d9344afc
Jim Dowling <jdowling@sics.se>
2016-08-06 12:31:10 +0200

    role name

--- providers/config.rb
+++ providers/config.rb
@@ -3,7 +3,7 @@ action :add do
   cluster = "#{node.kagent.cluster}"
   service = "#{new_resource.service}"
   role = "#{new_resource.role}"
-  section="#{role}"
+  section="#{cluster}-#{service}-#{role}"
   Chef::Log.info "Loaded kagent services ini-file #{ini_file} with : #{section}"
 
   if ini_file.has_section?(section)
