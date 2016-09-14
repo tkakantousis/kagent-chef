f0ab2d92fbad0e426aa3d27cb7bfa97547abdb08
Jim Dowling <jdowling@sics.se>
2016-09-14 13:28:52 +0200

    resolve conflicts

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-# Default values for configuration parameters
-default.kagent.run_as_user                 = "root"
-default.kagent.dir                         = "/var/lib"
-default.kagent.base_dir                    = "#{node.kagent.dir}/kagent"
-
-default.kagent.group_name                  = "group1"
-
-# Username/Password for connecting to the HopDashboard
-default.kagent.rest_api.user               = "kagent@sics.se"
-default.kagent.rest_api.password           = "kagent"
-
-# API calls
-default.kagent.dashboard.api.register      = "/api/agent/register"
-default.kagent.dashboard.api.login 	   = "/api/auth/login"
-
-# Username/Password for the dashboard connect ing to this agent
-default.kagent.dashboard.user              = "admin@kth.se"
-default.kagent.dashboard.password          = "admin"
-=======
-
 # Default values for configuration parameters
 default.kagent.version                     = "0.1.0"
 default.kagent.user                        = "kagent"
@@ -48,33 +27,21 @@ default.kagent.dashboard.password          = "admin"
 # Username/Password for the keystore
 
 default.hopsworks.master.password          = "adminpw"
->>>>>>> 6b3df193772b0d7b215534105aebca8ed1c3cce2
 
 # Agent's local certificate for SSL connections
 default.kagent.certificate_file            = "server.pem"
 
 # dashboard ip:port endpoint
-<<<<<<< HEAD
-# default.kagent.dashboard.port            = ""
-=======
-# default.kagent.dashboard.port         = ""
 default.kagent.dashboard.ip                = "10.0.2.15"
->>>>>>> 6b3df193772b0d7b215534105aebca8ed1c3cce2
 default.kagent.dashboard.port              = "8080"  
 default.kagent.dashboard_app               = "hopsworks"
 
 
 # local settings for agent
 default.kagent.port                        = 8090
-<<<<<<< HEAD
-default.kagent.heartbeat_interval          = 10
-default.kagent.watch_interval              = 2
-default.kagent.pid_file                    = node.kagent.base_dir + "/hop-agent.pid"
-=======
 default.kagent.heartbeat_interval          = 3
 default.kagent.watch_interval              = 2
 default.kagent.pid_file                    = node.kagent.base_dir + "/kagent.pid"
->>>>>>> 6b3df193772b0d7b215534105aebca8ed1c3cce2
 default.kagent.logging_level               = "INFO"
 default.kagent.max_log_size                = "10000000"
 
