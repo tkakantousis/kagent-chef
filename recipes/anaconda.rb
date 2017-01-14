158a04ec7b229d79e7cf9a994e1566a5fd1246f0
Jim Dowling <jdowling@sics.se>
2017-01-14 10:34:52 +0100

    syntax error

--- recipes/anaconda.rb
+++ recipes/anaconda.rb
@@ -13,7 +13,7 @@ node.override.anaconda.flavor = "x86_64"
 node.override.anaconda.python = "python2"
 node.override.anaconda.install_root = node.anaconda.dir
 
-include_repipe "anaconda::python_workaround"
+include_recipe "anaconda::python_workaround"
 
 include_recipe "anaconda::default"
 
