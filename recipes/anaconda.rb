611aad77f1fd01fb22e13313aa8da8478b1aa135
Jim Dowling <jdowling@sics.se>
2017-01-18 23:45:47 +0100

    set default conda packages

--- recipes/anaconda.rb
+++ recipes/anaconda.rb
@@ -42,6 +42,13 @@ bash "update_conda" do
 #   #{node.anaconda.base_dir}/bin/conda update conda -y -q
 #   #{node.anaconda.base_dir}/bin/conda update anaconda -y -q
 # This is the hopsworks-wide default environment that all projects will clone their environment from.
-   #{node.anaconda.base_dir}/bin/conda create -n hopsworksconda -y -q --file #{node.kagent.base_dir}/anaconda/spec-file.txt
+   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages numpy 
+   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages hdfs3
+   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages scikit-learn 
+   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages matplotlib 
+   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages pandas 
+   #{node.anaconda.base_dir}/bin/conda create -n hopsworksconda -y -q 
+
+# --file #{node.kagent.base_dir}/anaconda/spec-file.txt
  EOF
 end
