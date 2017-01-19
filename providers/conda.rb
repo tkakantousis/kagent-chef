bc0cf9007d97cd72939b0efc133f83e47b7c482c
Jim Dowling <jdowling@sics.se>
2017-01-19 08:33:32 +0100

    upgrade anaconda and conda

--- providers/conda.rb
+++ providers/conda.rb
@@ -1,8 +1,6 @@
 action :config do
 
 # This is the hopsworks-wide default environment that all projects will clone their environment from.
-#   #{node.anaconda.base_dir}/bin/conda update conda -y -q
-#   #{node.anaconda.base_dir}/bin/conda update anaconda -y -q
 #   #{node.anaconda.base_dir}/bin/conda create -n hopsworksconda -y -q 
 # --file #{node.kagent.base_dir}/anaconda/spec-file.txt
 
@@ -10,6 +8,8 @@ bash "update_conda_root" do
   user "root"
   group "root"
   code <<-EOF
+   #{node.anaconda.base_dir}/bin/conda update conda -y -q
+   #{node.anaconda.base_dir}/bin/conda update anaconda -y -q
    #{node.anaconda.base_dir}/bin/conda config --add create_default_packages numpy 
    #{node.anaconda.base_dir}/bin/conda config --add create_default_packages hdfs3
    #{node.anaconda.base_dir}/bin/conda config --add create_default_packages scikit-learn 
