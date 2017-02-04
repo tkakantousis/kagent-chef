78bad9d33cebee4d851453780b43175bc2aa8402
karamel <jdowling@sics.se>
2017-02-04 07:05:43 +0100

    install anaconda default env

--- providers/conda.rb
+++ providers/conda.rb
@@ -5,18 +5,32 @@ action :config do
 # --file #{node.kagent.base_dir}/anaconda/spec-file.txt
 
 bash "update_conda_root" do
-  user "root"
-  group "root"
+  user node.kagent.user
+  group node.kagent.group
+  timeout 7200
   code <<-EOF
    #{node.anaconda.base_dir}/bin/conda update conda -y -q
    #{node.anaconda.base_dir}/bin/conda update anaconda -y -q
-   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages numpy 
-   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages hdfs3
-   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages scikit-learn 
-   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages matplotlib 
-   #{node.anaconda.base_dir}/bin/conda config --add create_default_packages pandas 
  EOF
 end
+
+   # #{node.anaconda.base_dir}/bin/conda config --add create_default_packages numpy 
+   # #{node.anaconda.base_dir}/bin/conda config --add create_default_packages hdfs3
+   # #{node.anaconda.base_dir}/bin/conda config --add create_default_packages scikit-learn 
+   # #{node.anaconda.base_dir}/bin/conda config --add create_default_packages matplotlib 
+   # #{node.anaconda.base_dir}/bin/conda config --add create_default_packages pandas 
+
+
+bash 'create_default_anaconda_env' do
+  user node.kagent.user
+  group node.kagent.group
+  timeout 7200
+  code <<-EOF
+     #{node.kagent.base_dir}/bin/anaconda_env.sh #{node.kagent.user} install
+    EOF
+  not_if "source <%= node.anaconda.base_dir %>/bin/activate #{node.kagent.user}"
+end
+
 
 # bash "update_conda" do
 #   user node.kagent.user
--- recipes/anaconda.rb
+++ recipes/anaconda.rb
@@ -37,12 +37,3 @@ kagent_conda "packages" do
 end
 
 
-bash 'create_default_anaconda_env' do
-  user node.kagent.user
-  code <<-EOF
-     #{node.kagent.base_dir}/bin/anaconda_env.sh #{node.kagent.user} install
-    EOF
-  not_if "source <%= node.anaconda.base_dir %>/bin/activate #{node.kagent.user}"
-end
-
-
