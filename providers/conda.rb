76a23652c8f03556389a7d7baa5cba2c6b494414
karamel <jdowling@sics.se>
2017-02-04 10:51:44 +0100

    fix]

--- providers/conda.rb
+++ providers/conda.rb
@@ -4,31 +4,37 @@ action :config do
 #   #{node.anaconda.base_dir}/bin/conda create -n hopsworksconda -y -q 
 # --file #{node.kagent.base_dir}/anaconda/spec-file.txt
 
-
+execute "change_owner_to_kagent" do
+  user "root"
+  command "chown -R #{node.kagent.user}:#{node.kagent.group} #{node.anaconda.home}"
+end
+  
 execute "update_conda" do
   user "root"
-  command "su anaconda -c \"#{node.anaconda.base_dir}/bin/conda update conda -y -q\""
+  command "su #{node.kagent.user} -c \"#{node.anaconda.base_dir}/bin/conda update conda -y -q\""
 end
 
 execute "update_anconda" do
   user "root"
-  command "su anaconda -c \"#{node.anaconda.base_dir}/bin/conda update anaconda -y -q\""
+  command "su #{node.kagent.user} -c \"#{node.anaconda.base_dir}/bin/conda update anaconda -y -q\""
 end
+
+#
+# Install libraries into the root environment
+#
+for lib in node.anaconda.default_libs do
+  execute "install_anconda_default_libs" do
+  user "root"
+    command "su #{node.anaconda.user} -c \"#{node.anaconda.base_dir}/bin/conda install -q -y #{lib}\""
+  end
+end
+
 
 execute "create_base" do
   user "root"
-  command "su #{node.kagent.user} -c \"#{node.anaconda.base_dir}/bin/conda create -n #{node.kagent.user} --clone=root\""
+  command "su #{node.kagent.user} -c \"#{node.anaconda.base_dir}/bin/conda create -n #{node.kagent.user}\""
   not_if "test -d /home/#{node.kagent.user}/.conda/envs/#{node.kagent.user}"
 end
 
-
-for lib in node.anaconda.default_libs do
-  bash "install_anconda_default_libs_#{lib}" do
-  user node.kagent.user
-    code <<-EOH
-        #{node.anaconda.base_dir}/bin/conda install --name #{node.kagent.user} -q -y #{lib}"
-  EOH
-  end
-end
 
 end
