6c453091f7478c0c6c6e3a24752694528a708d05
Jim Dowling <jdowling@sics.se>
2017-03-06 14:21:50 +0100

    attrs update

--- metadata.rb
+++ metadata.rb
@@ -31,6 +31,10 @@ attribute "kagent/user",
           :description => "Username to run kagent as",
           :type => 'string'
 
+attribute "kagent/group",
+          :description => "group to run kagent as",
+          :type => 'string'
+
 attribute "kagent/dir",
           :description => "Installation directory for kagent",
           :type => 'string'
