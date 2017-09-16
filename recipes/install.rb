befc09d84668af2af726f06de6b284ee89d43efc
karamel <jdowling@sics.se>
2017-09-16 23:20:19 +0200

    needed to upgrade pip installations for dirty machines

--- recipes/install.rb
+++ recipes/install.rb
@@ -121,55 +121,55 @@ bash "install_python" do
   user 'root'
   ignore_failure true
   code <<-EOF
-  sudo -H pip install inifile
-  sudo -H pip install requests
-#  sudo -H pip install bottle
-#  sudo -H pip install CherryPy
-  sudo -H pip install pyOpenSSL
-  sudo -H pip install netifaces
-  sudo -H pip install IPy
-  sudo -H pip install pexpect
-#  sudo -H pip install cherrypy-wsgiserver
-#  sudo -H pip install wsgiserver
+  sudo -H pip install --upgrade inifile
+  sudo -H pip install --upgrade requests
+  sudo -H pip install --upgrade bottle
+  sudo -H pip install --upgrade CherryPy
+  sudo -H pip install --upgrade pyOpenSSL
+  sudo -H pip install --upgrade netifaces
+  sudo -H pip install --upgrade IPy
+  sudo -H pip install --upgrade pexpect
+  sudo -H pip install --upgrade cherrypy-wsgiserver
+  sudo -H pip install --upgrade wsgiserver
 
  EOF
 end
 
 
 
-bottle="bottle-0.11.4"
-cookbook_file "/tmp/#{bottle}.tar.gz" do
-  source "#{bottle}.tar.gz"
-  owner node["kagent"]["user"]
-  group node["kagent"]["user"]
-  mode 0755
-  action :create_if_missing
-end
+# bottle="bottle-0.11.4"
+# cookbook_file "/tmp/#{bottle}.tar.gz" do
+#   source "#{bottle}.tar.gz"
+#   owner node["kagent"]["user"]
+#   group node["kagent"]["user"]
+#   mode 0755
+#   action :create_if_missing
+# end
 
-cherry="CherryPy-3.2.2"
-cookbook_file "/tmp/#{cherry}.tar.gz" do
-  source "#{cherry}.tar.gz"
-  owner node["kagent"]["user"]
-  group node["kagent"]["user"]
-  mode 0755
-end
+# cherry="CherryPy-3.2.2"
+# cookbook_file "/tmp/#{cherry}.tar.gz" do
+#   source "#{cherry}.tar.gz"
+#   owner node["kagent"]["user"]
+#   group node["kagent"]["user"]
+#   mode 0755
+# end
 
 
-bash "install_python" do
-  user "root"
-  code <<-EOF
-  cd /tmp
-  tar zxf "#{bottle}.tar.gz"
-  cd #{bottle}
-  python setup.py install
-  cd ..
-  tar zxf "#{cherry}.tar.gz"
-  cd #{cherry}
-  python setup.py install
-  cd ..
- EOF
-  not_if "python -m wsgiserver"
-end
+# bash "install_python" do
+#   user "root"
+#   code <<-EOF
+#   cd /tmp
+#   tar zxf "#{bottle}.tar.gz"
+#   cd #{bottle}
+#   python setup.py install
+#   cd ..
+#   tar zxf "#{cherry}.tar.gz"
+#   cd #{cherry}
+#   python setup.py install
+#   cd ..
+#  EOF
+#   not_if "python -m wsgiserver"
+# end
 
 
 # ubuntu python-mysqldb package install only works if we first run "apt-get update; apt-get upgrade"
