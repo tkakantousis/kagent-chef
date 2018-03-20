4c9ef529ebba047816333058fff2aa1acc743892
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2018-03-20 09:36:38 +0100

    [HOPSWORKS-502]  Add command ID when doing certificate rotation (#35)

--- providers/keys.rb
+++ providers/keys.rb
@@ -7,7 +7,7 @@ action :csr do
     code <<-EOF
       set -eo pipefail
       export PYTHON_EGG_CACHE=/tmp
-      #{node["kagent"]["certs_dir"]}/csr.py --initialize
+      #{node["kagent"]["certs_dir"]}/csr.py operation initialize
   EOF
     not_if { ::File.exists?( "#{node["kagent"]["keystore_dir"]}/node_client_truststore.jks" ) }
   end
