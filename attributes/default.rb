34e4a0c4f72c64166ecfab7c973af28814495e7b
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-12-17 11:56:47 +0100

    Release version: 0.7

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,10 +9,10 @@ default["install"]["addhost"]                      = "false"
 default["install"]["current_version"]              = ""
 
 # Update target
-default["install"]["version"]                      = "0.7.0-SNAPSHOT"
+default["install"]["version"]                      = "0.8.0-SNAPSHOT"
 
 # List of released versions
-default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1"
+default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0,0.6.1,0.7.0"
 
 # hops-util-py version, when value is "master" install from git, when value is "0.6.0.0" do pip install hops==0.6.0.0
 default["kagent"]["hops-util-py-version"]          = "master"
