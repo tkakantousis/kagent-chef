c5f2672263db4fa6cad0816efd655a66fa136a9a
ErmiasG <ermiasg@kth.se>
2020-01-24 18:02:49 +0100

    [#34] make kagent use service login end-point (#129)

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,7 @@ default["kagent"]["certs_dir"]                     = "#{node["kagent"]["dir"]}/h
 
 # API calls
 default["kagent"]["dashboard"]["api"]["register"]  = "api/agentresource?action=register"
-default["kagent"]["dashboard"]["api"]["login"]     = "api/auth/login"
+default["kagent"]["dashboard"]["api"]["login"]     = "api/auth/service"
 default["kagent"]["dashboard"]["api"]["ca_host"]   = "v2/certificate/host"
 default["kagent"]["dashboard"]["api"]["heartbeat"] = "api/agentresource?action=heartbeat"
 default["kagent"]["dashboard"]["api"]["alert"]     = "api/agentresource/alert"
