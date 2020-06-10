e2e80a254badfc56b3eebb651c5760e44501b41c
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-06-10 12:06:39 +0200

    Move download_url to conda

--- attributes/default.rb
+++ attributes/default.rb
@@ -70,7 +70,6 @@ default["public_ips"]                              = ['10.0.2.15']
 default["private_ips"]                             = ['10.0.2.15']
 default["gateway_ips"]                             = ['10.0.2.2']
 
-default["download_url"]                       = "https://repo.hops.works/master"
 default["systemd"]                            = "true"
 
 default["vagrant"]                            = "false"
